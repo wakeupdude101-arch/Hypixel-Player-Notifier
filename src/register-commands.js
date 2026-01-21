e861b9f662adfa1a14fd72ec0d8f763cf6afd04b
WakeUpDude <wakeupdude101@gmail.com>
2026-01-21 11:34:42 +0545

    Added Slash Commands

---
+++ src/register-commands.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+];
+
+(async () => {
+    try {
+
+    }catch (error) {
+        console.log(`There was an error: ${error}`);
+    }
+})();
+    
