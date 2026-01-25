1242db68b68d846b67f3c4b9fa48e3532f6ab037
WakeUpDude <wakeupdude101@gmail.com>
2026-01-25 14:25:32 +0545

    Added slash commands

--- src/register-commands.js
+++ src/register-commands.js
@@ -8,9 +8,19 @@ const commands = [
     },
 ];
 
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
 (async () => {
     try {
+        console.log("Registering Slash commands");
 
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        );
+        console.log("slash commands were registered succesfully!");
     }catch (error) {
         console.log(`There was an error: ${error}`);
     }
