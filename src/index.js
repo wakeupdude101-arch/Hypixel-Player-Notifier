d20f67bc807261b23918a63500d4d726a78f65b9
WakeUpDude <wakeupdude101@gmail.com>
2026-01-26 09:32:58 +0545

    Added /ping command

--- src/index.js
+++ src/index.js
@@ -26,5 +26,11 @@ client.on('messageCreate', (message) => {
         message.react('🤨');
     };
 });
+client.on('interactionCreate', async (interaction) => {
+    if(!interaction.isChatInputCommand()) return;
 
+    if(interaction.commandName === 'ping'){
+        interaction.reply('Pong!');
+    }
+});
 client.login(process.env.TOKEN);
