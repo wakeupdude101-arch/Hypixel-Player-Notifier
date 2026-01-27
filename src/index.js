b0b96152cdc53da76db9b5c7014156117368cb37
WakeUpDude <wakeupdude101@gmail.com>
2026-01-27 12:43:23 +0545

    Added discord checker

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ client.on('interactionCreate', async (interaction) => {
     const statMessage = `${ign} is now offline!`;
 
     
-
+    async function lovely(){
     const API_KEY = process.env.HYPIXEL_API;
     const username = await fetch(`https://api.mojang.com/users/profiles/minecraft/${ign}`);
     const uuid = await username.json();
@@ -47,10 +47,16 @@ client.on('interactionCreate', async (interaction) => {
     const userData = await userStatus.json();
     const playerData = await fetch(`https://api.hypixel.net/v2/player?key=${API_KEY}&uuid=${PLAYER_UUID}`);
     const pData = await playerData.json();
-   //console.log(pData);
+    const lastLogin = pData.player.lastLogin;
+    const firstLogin = pData.player.firstLogin;
+    const lastLogout = pData.player.mostRecentGameType;
+    const discordName = pData.player?.socialMedia?.links?.DISCORD || 'Not Linked!';
+    //console.log(pData.player.firstLogin);
     
     const onlineMessage = `${uuid.name} is now Online`;
     const offlineMessage = `${uuid.name} is now offline`;
+    return {uuid, userData, PLAYER_UUID, pData, lastLogin, firstLogin, lastLogout, statMessage, onlineMessage, offlineMessage, bothMessage, singleMessage, discordName};
+    };
 
 
  
@@ -61,37 +67,45 @@ client.on('interactionCreate', async (interaction) => {
             interaction.reply({ ephemeral: true, content: 'Pong!'});
         break;
         case 'status':
-            if (!uuid || !uuid.id){
+        const Data = await lovely();
+        const firstTime = `<t:${Math.floor(Data.firstLogin/1000)}:F>`;
+        const secondTime = `<t:${Math.floor(Data.lastLogin/1000)}:F>`;
+        const betterTicks = `\`${Data.discordName}\``;
+
+            if (!Data.uuid || !Data.uuid.id){
                interaction.reply({ ephemeral: true, content: "Player doesn't exists" });
-            }else if(userData.session.online === false ){
-                const statusEmbed = new EmbedBuilder().setColor('Red').setTitle(offlineMessage).setFields(
-                { name: 'Status: ', value: '🟥 Offline', inline: true },
-                { name: 'Game: ', value: 'Skyblock'},
-                { name: 'Last Login', value: `${pData.lastlogin}`, inline: true },
-                { name: 'First Login', value: `${pData.firstlogin}`, inline: true },
+               return;
+            }else if(Data.userData.session.online === false ){
+                const statusEmbed = new EmbedBuilder().setColor('Red').setTitle(Data.offlineMessage).setFields(
+                { name: 'Status: ', value: '🟥 Offline'},
+                { name: 'Discord ', value: betterTicks},
+                { name: 'Last Online', value: secondTime},
+                { name: 'First Login', value: firstTime },
             ).setThumbnail('https://i.imgur.com/LUn8WDe.png').setTimestamp().setFooter({ text: 'by @wakeupdude.', iconURL: 'https://imgur.com/a/SV1QkKc' });
                 interaction.reply({ ephemeral: true, embeds: [statusEmbed] });
             }else{
-                const statusEmbed = new EmbedBuilder().setColor('Green').setTitle(onlineMessage).setFields(
+                const statusEmbed = new EmbedBuilder().setColor('Green').setTitle(Data.onlineMessage).setFields(
                 { name: 'Status: ', value: '🟩 Online' },
-                { name: 'Game: ', value: 'Skyblock'},
-                { name: 'Last Login', value: '```2 months ago```', inline: true },
-                { name: 'First Login', value: '```2 months ago```', inline: true },
+                { name: 'Discord: ', value: betterTicks},
+                { name: 'Game: ', value: Data.lastLogout},
+                { name: 'Last Online', value: secondTime},
+                { name: 'First Login', value: firstTime},
             ).setThumbnail('https://i.imgur.com/LUn8WDe.png').setTimestamp().setFooter({ text: 'by @wakeupdude.', iconURL: 'https://imgur.com/a/SV1QkKc' });
             interaction.reply({ ephemeral: true, embeds: [statusEmbed] });
             }
         break;
         case 'notify':
+        const Data2 = await lovely();
            if(ip === undefined){
-            const notifyEmbed = new EmbedBuilder().setColor('Random').setTitle(singleMessage).setFields(
-                { name: 'Uuid: ', value: userData.uuid },
+            const notifyEmbed = new EmbedBuilder().setColor('Random').setTitle(Data2.singleMessage).setFields(
+                { name: 'Uuid: ', value: Data2.userData.uuid },
                 { name: 'You will be notified on next login.', value: '\u200b' },
             ).setThumbnail('https://i.imgur.com/LUn8WDe.png').setTimestamp().setFooter({ text: 'by @wakeupdude.', iconURL: 'https://imgur.com/a/SV1QkKc' });
             interaction.reply({ ephemeral: true, embeds: [notifyEmbed] });
         }else{
-            const notifyEmbed = new EmbedBuilder().setColor('Random').setTitle(bothMessage).setFields(
-                { name: 'Uuid: ', value: userData.uuid },
-                { name: 'You will be notified on next login.', value: '\u200b' },
+            const notifyEmbed = new EmbedBuilder().setColor('Random').setTitle(Data2.bothMessage).setFields(
+                { name: 'Uuid: ', value: Data2.userData.uuid },
+                { name: 'You will be notified on next login.', value: '' },
             ).setThumbnail('https://i.imgur.com/LUn8WDe.png').setTimestamp().setFooter({ text: 'by @wakeupdude.', iconURL: 'https://imgur.com/a/SV1QkKc' });
             interaction.reply({ ephemeral: true, embeds: [notifyEmbed] });
         }
